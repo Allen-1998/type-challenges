e58fcc341bac526fad312ca04e8eda1ad28bec53
allen <272970666@qq.com>
2022-07-18 18:11:33 +0800

    update

--- questions/00017-hard-currying-1/template.ts
+++ questions/00017-hard-currying-1/template.ts
@@ -1,1 +1,13 @@
-declare function Currying(fn: any): any
+type CurriedFn<Fn> = Fn extends (...args: infer Args) => infer R
+  ? Args extends [infer F, ...infer Rest]
+    ? (arg: F) => CurriedFn<(...rest: Rest) => R>
+    : R
+  : never
+
+declare function Currying<Fn>(fn: Fn): CurriedFn<Fn>
+
+const add = (a: number, b: number) => a + b
+const three = add(1, 2)
+
+const curriedAdd = Currying(add)
+const five = curriedAdd(2)(3)
--- questions/00020-medium-promise-all/template.ts
+++ questions/00020-medium-promise-all/template.ts
@@ -1,1 +1,11 @@
-declare function PromiseAll(values: any): any
+declare function PromiseAll<T extends any[]>(
+  values: readonly [...T]
+): Promise<{ [K in keyof T]: T[K] extends Promise<infer R> ? R : T[K] }>
+
+const promise1 = Promise.resolve(3)
+const promise2 = 42
+const promise3 = new Promise<string>((resolve) => {
+  setTimeout(resolve, 100, "foo")
+})
+
+const p = Promise.all([promise1, promise2, promise3] as const)
