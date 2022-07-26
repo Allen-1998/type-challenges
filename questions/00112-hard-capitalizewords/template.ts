c1aaf00e9300b95e9ca20f2c0051e3e61a05abac
allen <272970666@qq.com>
2022-07-26 14:56:30 +0800

    update

--- questions/00112-hard-capitalizewords/template.ts
+++ questions/00112-hard-capitalizewords/template.ts
@@ -1,1 +1,16 @@
-type CapitalizeWords<S extends string> = any
+type AlphabetLowerCase =
+'a' | 'b' | 'c' | 'd' | 'e' |
+'f' | 'g' | 'h' | 'i' | 'j' |
+'k' | 'l' | 'm' | 'n' | 'o' |
+'p' | 'q' | 'r' | 's' | 't' |
+'u' | 'v' | 'w' | 'x' | 'y' |
+'z'
+
+type Alphabet = AlphabetLowerCase | Uppercase<AlphabetLowerCase>
+
+type CapitalizeWords<S extends string, Prev extends string=' '> =
+  S extends `${infer L}${infer Rest}`
+    ? `${Prev extends Alphabet ? L : Capitalize<L>}${CapitalizeWords<Rest, L>}`
+    : S
+// TODO 待二刷，这里是一个黑魔法，注释掉下面这一行会导致倒数第二个case不通过,提示类型实例化过深，且可能无限
+type test = CapitalizeWords<'🤣qq'>
