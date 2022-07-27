21bd0baa0136fca0a157a185a4b22303f83afb74
allen <272970666@qq.com>
2022-07-27 18:48:55 +0800

    update

--- questions/00114-hard-camelcase/template.ts
+++ questions/00114-hard-camelcase/template.ts
@@ -1,1 +1,14 @@
-type CamelCase<S extends string> = any
+type CamelCase<S extends string> = Uncapitalize<BigCamelCase<S>>
+
+type UppercaseOnlyFirst<S extends string> = S extends `${infer P}${infer T}` ? `${Uppercase<P>}${Lowercase<T>}` : Uppercase<S>
+
+type BigCamelCase<S extends string> = S extends `${infer T}_${infer P}` ? `${UppercaseOnlyFirst<T>}${BigCamelCase<UppercaseOnlyFirst<P>>}` : UppercaseOnlyFirst<S>
+
+{
+    // 更优解
+    type CamelCase<S extends string> = S extends Lowercase<S>
+      ? S extends `${infer F}_${infer RF}${infer R}`
+        ? `${F}${Uppercase<RF>}${CamelCase<R>}`
+        : S
+      : CamelCase<Lowercase<S>>
+}
