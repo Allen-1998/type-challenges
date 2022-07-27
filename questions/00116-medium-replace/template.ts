5f133b1658565da8545e5d154a093ce3a63233ea
allen <272970666@qq.com>
2022-07-27 19:03:34 +0800

    update

--- questions/00116-medium-replace/template.ts
+++ questions/00116-medium-replace/template.ts
@@ -1,1 +1,5 @@
-type Replace<S extends string, From extends string, To extends string> = any
+type Replace<S extends string, From extends string, To extends string> = S extends `${infer T}${From}${infer P}` ? `${T}${From extends '' ? From : To}${P}` : S
+
+{
+    type test = Replace<'foobarbar', '', 'foo'>
+}
--- questions/00119-medium-replaceall/template.ts
+++ questions/00119-medium-replaceall/template.ts
@@ -1,1 +1,2 @@
-type ReplaceAll<S extends string, From extends string, To extends string> = any
+type ReplaceAll<S extends string, From extends string, To extends string> = S extends `${infer T}${From}${infer P}` ? `${T}${From extends '' ? From : To}${ReplaceAll<P, From, To>}` : S
+
