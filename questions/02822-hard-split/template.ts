fe64a92094e7246fa99bafdeb37df2bebf8cf2ff
allen <272970666@qq.com>
2022-07-28 17:20:39 +0800

    update

--- questions/02822-hard-split/template.ts
+++ questions/02822-hard-split/template.ts
@@ -1,1 +1,14 @@
-type Split<S extends string, SEP extends string> = any
+type Split<S extends string, SEP extends string> =
+ S extends ""
+   ? SEP extends ""
+     ? []
+     : [""]
+   : S extends `${infer K}${SEP}${infer V}`
+     ? [K, ...Split<V, SEP>]
+     : S extends `${infer _}`
+       ? [S]
+       : string[]
+
+{
+  type a = Split<"Hi! How are you?", "">
+}
