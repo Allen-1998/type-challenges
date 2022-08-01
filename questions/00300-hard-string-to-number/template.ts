e61a673187f8d3ce4175a8a13bc71eb122499eba
allen <272970666@qq.com>
2022-08-01 13:48:56 +0800

    update

--- questions/00300-hard-string-to-number/template.ts
+++ questions/00300-hard-string-to-number/template.ts
@@ -1,1 +1,3 @@
-type ToNumber<S extends string> = any
+type ToNumber<S extends string, R extends unknown[] = []> = S extends `${number}`
+  ? S extends `${R["length"]}` ? R["length"] : ToNumber<S, [...R, unknown]>
+  : never
