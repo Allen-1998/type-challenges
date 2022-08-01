8b623d979e965c1083c464ffd323bf550b4a8b2d
allen <272970666@qq.com>
2022-08-01 10:06:18 +0800

    update

--- questions/00651-hard-length-of-string-2/template.ts
+++ questions/00651-hard-length-of-string-2/template.ts
@@ -1,1 +1,6 @@
-type LengthOfString<S extends string> = number
+type LengthOfString2<
+  S extends string,
+  T extends string[] = []
+> = S extends `${infer F}${infer R}`
+  ? LengthOfString<R, [...T, F]>
+  : T['length']
