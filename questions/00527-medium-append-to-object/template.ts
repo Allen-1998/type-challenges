8ff1b90484eb5bfd7c45f1a217f72dd9cf5ac622
allen <272970666@qq.com>
2022-08-03 17:04:56 +0800

    update

--- questions/00527-medium-append-to-object/template.ts
+++ questions/00527-medium-append-to-object/template.ts
@@ -1,1 +1,11 @@
-type AppendToObject<T, U, V> = any
+type AppendToObject<T, U extends string, V> = {
+  [K in keyof T | U]: K extends U ? V : K extends keyof T ? T[K] : never
+}
+
+{
+  type test1 = {
+    key: "cat"
+    value: "green"
+  }
+  type a = AppendToObject<test1, "home", boolean>
+}
