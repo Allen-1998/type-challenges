0967a7571e93bbc45c870a3de80db9004a23b8bc
allen <272970666@qq.com>
2022-08-03 15:28:00 +0800

    update

--- questions/00459-medium-flatten/template.ts
+++ questions/00459-medium-flatten/template.ts
@@ -1,1 +1,5 @@
-type Flatten = any
+type Flatten<T extends unknown[]> = T extends [infer A, ...infer B]
+  ? A extends unknown[]
+    ? [...Flatten<A>, ...Flatten<B>]
+    : [A, ...Flatten<B>]
+  : []
